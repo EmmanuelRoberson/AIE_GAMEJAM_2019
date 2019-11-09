2e6990a69d37f8b195536965c157549cd1cf4acb
Emmanuel Roberson <emmanuel.roberson3@gmail.com>
2019-11-08 21:55:52 -0600

    Added working player movement, camera rotation

---
+++ Assets/Scripts/BasicMovementBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements;
+using UnityEngine;
+
+public class BasicMovementBehaviour : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += -transform.forward * speed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up);
+        }
+
+        
+        
+    }
+}
