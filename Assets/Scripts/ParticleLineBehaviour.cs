d9e19d20b74d6c9566cafeeba6120f0e9a39f1d3
Emmanuel Roberson <emmanuel.roberson3@gmail.com>
2019-11-09 10:32:57 -0600

    simple scene changes

--- Assets/Scripts/ParticleLineBehaviour.cs
+++ Assets/Scripts/ParticleLineBehaviour.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class ParticleLineBehaviour : MonoBehaviour
@@ -7,16 +6,23 @@ public class ParticleLineBehaviour : MonoBehaviour
     public Transform originPoint;
 
     public Transform destinationPoint;
+
+    private ParticleSystem ps;
     // Start is called before the first frame update
     void Start()
     {
+        ps = GetComponent<ParticleSystem>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         transform.position = originPoint.position;
         transform.LookAt(destinationPoint);
+
+        var main = ps.main;
+        main.startSpeed = Math.Abs(Vector3.Distance(this.transform.position, destinationPoint.position))/ps.main.duration;
     }
 }
